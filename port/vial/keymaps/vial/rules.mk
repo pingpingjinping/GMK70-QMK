762086cdaa4113ad954e6bd9b51a2023c6c9cf27
pingpingjinping <pingpingjinping@gmail.com>
2026-09-24 12:13:17 +0900

    Configure minimal Vial feature set

---
+++ port/vial/keymaps/vial/rules.mk
@@ -0,0 +1,15 @@
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
+
+# Keep the first port small enough for the WB32FQ95 96 KiB flash.
+VIALRGB_ENABLE = no
+QMK_SETTINGS = no
+CAPS_WORD_ENABLE = no
+LAYER_LOCK_ENABLE = no
+REPEAT_KEY_ENABLE = no
+
+# Keep the Vial features we actually want to validate first.
+TAP_DANCE_ENABLE = yes
+COMBO_ENABLE = yes
+KEY_OVERRIDE_ENABLE = yes
+ENCODER_MAP_ENABLE = yes
